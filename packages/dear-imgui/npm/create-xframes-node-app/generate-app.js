e691c687e2beadb96c0087d5f3af4eeab2a43e91
andreamancuso <and.mancuso@googlemail.com>
2024-11-08 12:14:45 +0100

    Adding very basic, crude create-xframes-node-app package

---
+++ packages/dear-imgui/npm/create-xframes-node-app/generate-app.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+import os from "os";
+import path from "path";
+import fs from "fs";
+import { execSync, execFileSync } from "child_process";
+import { input } from "@inquirer/prompts";
+
+const projectName = (
+  await input({ message: "Enter your project name" })
+).trim();
+
+if (projectName === "") {
+  console.log("Please enter a project name");
+  process.exit(1);
+}
+
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const srcPath = path.join(projectPath, "src");
+const indexJsPath = path.join(srcPath, "index.js");
+
+try {
+  fs.mkdirSync(projectPath);
+} catch (err) {
+  if (err.code === "EEXIST") {
+    console.log(
+      `The folder ${projectName} already exists, please choose a different name.`
+    );
+  } else {
+    console.log(error);
+  }
+  process.exit(1);
+}
+
+try {
+  fs.mkdirSync(srcPath);
+} catch (err) {
+  console.log(`Unable create src path: ${error}`);
+  process.exit(1);
+}
+
+const packageJson = {
+  name: projectName,
+  version: "0.1.0",
+  private: true,
+  dependencies: {
+    "@xframes/common": "0.0.13",
+    "@xframes/node": "0.0.18",
+    react: "18.3.1",
+  },
+  scripts: {
+    start: "node ./src/index.js",
+  },
+};
+fs.writeFileSync(
+  path.join(projectPath, "package.json"),
+  JSON.stringify(packageJson, null, 2) + os.EOL
+);
+
+const indexJsUrl =
+  "https://raw.githubusercontent.com/andreamancuso/xframes/refs/heads/main/packages/dear-imgui/npm/create-xframes-node-app/src/index.js";
+
+console.log("Downloading source file...");
+
+execFileSync("curl", ["-o", indexJsPath, "--silent", "-L", indexJsUrl], {
+  encoding: "utf8",
+});
+
+process.chdir(projectPath);
+
+console.log("Running npm install...");
+
+execSync("npm install");
+
+process.chdir(currentPath);
+
+console.log("Assuming no errors occurred, you're all done. You may now run");
+
+console.log(`cd ${projectName}`);
---
+++ packages/dear-imgui/npm/create-xframes-node-app/index.js
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import * as React from "react";
+import { theme2 } from "./themes";
+import { render, XFrames } from "@xframes/node";
+
+const fontDefs = {
+  defs: [{ name: "roboto-regular", sizes: [16, 18, 20, 24] }]
+    .map(({ name, sizes }) => sizes.map((size) => ({ name, size })))
+    .flat(),
+};
+
+const assetsBasePath = resolve("./assets");
+
+const App = () => (
+  <XFrames.Node root style={{ height: "100%" }}>
+    <XFrames.UnformattedText text="Hello, world" />
+  </XFrames.Node>
+);
+
+render(App, assetsBasePath, fontDefs, theme2);
